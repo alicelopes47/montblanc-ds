ee3b577dfcdb963f906d80217414d136936695fd
Alice Lopes <lopesalice4745@gmail.com>
2024-07-09 00:59:15 -0300

    chore: Update Storybook configuration and styles for Header component

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -3,6 +3,22 @@ import '../src/app/globals.css';
 
 const preview: Preview = {
   parameters: {
+    ScreenOrientation: {
+      type: 'portrait',
+    },
+    backgrounds: {
+      default: 'light',
+      values: [
+        {
+          name: 'light',
+          value: '#ffffff',
+        },
+        {
+          name: 'dark',
+          value: '#000000',
+        },
+      ],
+    },
     controls: {
       matchers: {
         color: /(background|color)$/i,
